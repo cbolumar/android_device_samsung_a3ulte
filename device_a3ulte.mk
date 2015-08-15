ad835925c05b3df0a8f201d70fc45bb48ab91e7f
Hannu Markkula <hannu.markkula007007@gmail.com>
2015-08-15 10:54:55 +0300

    update
    Change-Id: Iffebd64e9e5dd46d0b445497bb974d2763bd8433

--- device_a3ulte.mk
+++ device_a3ulte.mk
@@ -148,7 +148,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
+#     frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     device/samsung/a3ulte/configs/media_codecs.xml:system/etc/media_codecs.xml \
     device/samsung/a3ulte/configs/media_profiles.xml:system/etc/media_profiles.xml
 
